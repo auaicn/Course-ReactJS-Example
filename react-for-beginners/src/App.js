5239b3f23dc746d08c8a1c6f98064aabff2717fd
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-05 07:00:30 +0900

    Study: Tried adding dependencies to React Effect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,16 +2,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setState] = useState(0);
-  const onClick = () => setState((prev) => prev + 1);
+  const [keyword, setKeyword] = useState("");
 
-  console.log("I run all the time");
+  const onClick = () => setState((previousValue) => previousValue + 1);
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("I run only once.");
   }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' or 'counter' changes.");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input value={keyword} type="text" placeholder="Search here" onChange={onChange} />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me!</button>
     </div>
