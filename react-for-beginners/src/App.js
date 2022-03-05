cb37426950cfd63d8db7d02d1c514a781df44b02
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-06 05:18:18 +0900

    Feat: Added router that resolves url

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  // Switch
+  return (
+    <Router>
+      {/* 한번에 하나의 Route 만 렌더링 하기 위함 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
